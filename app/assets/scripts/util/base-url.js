8d1c2142fa8725a52d3ffc5527832f7ec75f3f04
Stevan Postic <stevan.postic@cloudtech.company>
2017-11-01 15:13:59 +0100

    base_url

--- app/assets/scripts/util/base-url.js
+++ app/assets/scripts/util/base-url.js
@@ -1,2 +1,2 @@
 module.exports = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000' : 'https://rankingdigitalrights.org/index2017';
+  ? 'http://rdrbackup.cloudtech.company:3000' : 'https://rankingdigitalrights.org/index2017';
