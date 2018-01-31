af458b435cadc67369a98b5b33c43f05ff530af1
steva <postic.stevan@nsinfo.co.rs>
2018-01-31 15:05:07 +0100

    Compare page

--- app/assets/scripts/compare.js
+++ app/assets/scripts/compare.js
@@ -1,2 +1,20 @@
 var queue = require('queue-async');
 var $ = require('jquery');
+
+var Compare = require('./collections/compare');
+var CompareView = require('./views/compare');
+
+module.exports = function() {
+
+    var compare = new Compare();
+
+    var compareView = new CompareView({
+        collection: compare,
+    });
+
+    compare.fetch({
+        success: function () {
+            compareView.render();
+        }
+    });
+}
