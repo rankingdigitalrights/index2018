bebed0f5d4fdce3cfe9902521419a7dbb9657f6b
Stevan Postic <stevan.postic@cloudtech.company>
2018-01-31 22:24:56 +0100

    Compare page

--- app/assets/scripts/views/compare.js
+++ app/assets/scripts/views/compare.js
@@ -15,142 +15,157 @@ module.exports = Backbone.View.extend({
 
         var data = this.collection.models;
 
-        data.forEach(function (i, dd) {
-            console.info(i.attributes.name);
+        var $data = [];
+        data.forEach(function (i) {
+            $data.push({name:i.attributes.name,total_difference:i.attributes.total_difference});
         });
 
-        var margin = {
-                top: 10,
-                right: 10,
-                bottom: 20,
-                left: 30
-            },
-            width = 920 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
 
-        var y = d3.scale.linear()
-            .range([height, 0]);
+        $data.sort(function(a, b) {
+            return parseFloat(a.total_difference) - parseFloat(b.total_difference);
+        });
 
-        var x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .2);
-
-        var xAxisScale = d3.scale.linear()
-            .domain([1880, 2015])
-            .range([ 0, width]);
-
-        var xAxis = d3.svg.axis()
-            .scale(xAxisScale)
-            .orient("bottom")
-            .tickFormat(d3.format("d"));
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
-
-        var svg = d3.select("#compare--overview_chart").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            //console.info(data);
-            x.domain(data.map(function(d) {
-                return d.name;
-            }));
-            y.domain(d3.extent(data, function(d) {
-                return d.total_difference;
-            })).nice();
-
-            svg.selectAll(".bar")
-                .data(data)
-                .enter().append("rect")
-                .attr("class", function(d) {
-
-                    if (d.total_difference < 0){
-                        return "bar negative";
-                    } else {
-                        return "bar positive";
-                    }
-
-                })
-                .attr("data-yr", function(d){
-                    return d.name;
-                })
-                .attr("data-c", function(d){
-                    return d.total_difference;
-                })
-                .attr("title", function(d){
-                    return (d.name + ": " + d.total_difference + " °C")
-                })
-                .attr("y", function(d) {
-
-                    if (d.total_difference > 0){
-                        return y(d.total_difference);
-                    } else {
-                        return y(0);
-                    }
-
-                })
-                .attr("x", function(d) {
-                    return x(d.name);
-                })
-                .attr("width", x.rangeBand())
-                .attr("height", function(d) {
-
-                    console.info(d.name);
-
-                    return Math.abs(y(d.total_difference) - y(0));
-                })
-                .on("mouseover", function(d){
-                    // alert("Year: " + d.Year + ": " + d.Celsius + " Celsius");
-                    d3.select("#_yr")
-                        .text("Year: " + d.name);
-                    d3.select("#degrree")
-                        .text(d.total_difference + "°C");
-                });
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis);
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .append("text")
-                .text("°Celsius")
-                .attr("transform", "translate(15, 40), rotate(-90)")
-
-            svg.append("g")
-                .attr("class", "X axis")
-                .attr("transform", "translate(" + (margin.left - 6.5) + "," + height + ")")
-                .call(xAxis);
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .append("line")
-                .attr("y1", y(0))
-                .attr("y2", y(0))
-                .attr("x2", width);
-
-            svg.append("g")
-                .attr("class", "infowin")
-                .attr("transform", "translate(50, 5)")
-                .append("text")
-                .attr("id", "_yr");
-
-            svg.append("g")
-                .attr("class", "infowin")
-                .attr("transform", "translate(110, 5)")
-                .append("text")
-                .attr("id","degrree");
-
-
-
-
-        function type(d) {
-            d.total_difference = +d.total_difference;
-            return d;
-        }
-
+        $data.reverse();
+        
+        //var _data = [["A",0.012], ["B",-0.025], ["C",0.008], ["D",0.023], ["E",-0.009], ["F", 0.005]];
+        
+        d3.select("#compare--overview_chart")
+            .datum($data)
+            .call(columnChart()
+            .width(960)
+            .height(500)
+            .x(function(d, i) { return d.name; })
+            .y(function(d, i) { return d.total_difference; }));
 
     },
 });
+
+
+function columnChart() {
+  var margin = {top: 30, right: 10, bottom: 50, left: 50},
+      width = 420,
+      height = 420,
+      xRoundBands = 0.2,
+      xValue = function(d) { return d[0]; },
+      yValue = function(d) { return d[1]; },
+      xScale = d3.scale.ordinal(),
+      yScale = d3.scale.linear(),
+      yAxis = d3.svg.axis().scale(yScale).orient("left"),
+      xAxis = d3.svg.axis().scale(xScale);
+      
+
+  function chart(selection) {
+    selection.each(function(data) {
+
+      // Convert data to standard representation greedily;
+      // this is needed for nondeterministic accessors.
+      data = data.map(function(d, i) {
+        return [xValue.call(data, d, i), yValue.call(data, d, i)];
+      });
+    
+      // Update the x-scale.
+      xScale
+          .domain(data.map(function(d) { return d[0];} ))
+          .rangeRoundBands([0, width - margin.left - margin.right], xRoundBands);
+         
+
+      // Update the y-scale.
+      yScale
+          .domain(d3.extent(data.map(function(d) { return d[1];} )))
+          .range([height - margin.top - margin.bottom, 0])
+          .nice();
+          
+
+      // Select the svg element, if it exists.
+      var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // Otherwise, create the skeletal chart.
+      var gEnter = svg.enter().append("svg").append("g");
+      gEnter.append("g").attr("class", "bars");
+      gEnter.append("g").attr("class", "y axis");
+      gEnter.append("g").attr("class", "x axis");
+      gEnter.append("g").attr("class", "x axis zero");
+
+      // Update the outer dimensions.
+      svg .attr("width", width)
+          .attr("height", height);
+
+      // Update the inner dimensions.
+      var g = svg.select("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+     // Update the bars.
+      var bar = svg.select(".bars").selectAll(".bar").data(data);
+      bar.enter().append("rect");
+      bar.exit().remove();
+      bar .attr("class", function(d, i) { return d[1] < 0 ? "bar negative" : "bar positive"; })
+          .attr("x", function(d) { return X(d); })
+          .attr("y", function(d, i) { return d[1] < 0 ? Y0() : Y(d); })
+          .attr("width", xScale.rangeBand())
+          .attr("height", function(d, i) { return Math.abs( Y(d) - Y0() ); });
+
+    // x axis at the bottom of the chart
+     g.select(".x.axis")
+        .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
+        .call(xAxis.orient("bottom"));
+    
+    // zero line
+     g.select(".x.axis.zero")
+        .attr("transform", "translate(0," + Y0() + ")")
+        .call(xAxis.tickFormat("").tickSize(0));
+    
+    
+      // Update the y-axis.
+      g.select(".y.axis")
+        .call(yAxis);
+          
+    });
+  }
+
+
+// The x-accessor for the path generator; xScale ∘ xValue.
+  function X(d) {
+    return xScale(d[0]);
+  }
+
+  function Y0() {
+    return yScale(0);
+  }
+
+  // The x-accessor for the path generator; yScale ∘ yValue.
+  function Y(d) {
+    return yScale(d[1]);
+  }
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.x = function(_) {
+    if (!arguments.length) return xValue;
+    xValue = _;
+    return chart;
+  };
+
+  chart.y = function(_) {
+    if (!arguments.length) return yValue;
+    yValue = _;
+    return chart;
+  };
+
+  return chart;
+}
