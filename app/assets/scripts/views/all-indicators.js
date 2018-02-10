6ae8c258228e48725579f1be3fe2d9928b655c6f
Stevan Postic <stevan.postic@cloudtech.company>
2018-02-11 00:22:39 +0100

    Indicators

--- app/assets/scripts/views/all-indicators.js
+++ app/assets/scripts/views/all-indicators.js
@@ -1,40 +1,27 @@
 var $ = require('jquery');
 var _ = require('underscore');
-var resize = require('../util/resize');
-var IndicatorView = require('./indicator');
-var BaseChart = require('./base-chart');
+var Backbone = require('backbone');
+var template = require('../templates/indicators.tpl');
 
-module.exports = BaseChart.extend({
-  tagName: 'div',
+module.exports = Backbone.View.extend({
+
   initialize: function (options) {
     _.extend(this, options);
-    resize.attach(this.id, this.$el);
-    this.listenTo(resize, 'resize:' + this.id, this.handleResize);
-  },
-  render: function (id) {
-    var childViews = this.childViews;
-    var $el = this.$el;
-    var $parent = $('#' + id);
-    var width = $parent.width();
-    this.collection.each(function (model) {
-      var id = model.attributes.indicator;
-      var view = new IndicatorView({
-        id: id,
-        model: model,
-        width: width,
-        indicator_id: id
-      });
-      childViews.push(view);
-      $el.append(view.render());
-    });
-    $parent.append($el);
   },
 
-  handleResize: function (dimensions) {
-    this.childViews.forEach(function (view) {
-      if (view.handleResize) {
-        view.handleResize(dimensions);
-      }
+  render: function (el) {
+
+    var $el = el;
+    var $indicators = this.collection;
+    var $data = $indicators.filter(model => model.get('indicator').charAt(0) === el.charAt(0));
+    $data.forEach(function (item) {
+
+      $('#overview_list--'+$el).append(
+        template({name:item.attributes.name})
+      );
+
     });
-  }
+
+  },
+
 });
